19237d197a99f3717f4f8baff6c34e46fd1bbc9f
Ioan Dragan <ioan.dumitru.dragan@gmail.com>
2015-09-18 11:58:48 +0300

    Initial commit to the PiEstimator.java and documentation

---
+++ piEstimator/src/PiEstimator.java
@@ -0,0 +1,13 @@
+import org.apache.hadoop.fs.FileSystem;
+
+public class PiEstimator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
